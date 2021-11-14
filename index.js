d0f312c1e753753ac587a0da268ebc471b1b4753
m.jafari <mahsa.jafari38555@gmail.com>
2021-11-14 13:28:11 +0330

    add and remove

--- index.js
+++ index.js
@@ -2,7 +2,9 @@
 const inputValue = document.querySelector(".to-do-input");
 const todoButton = document.querySelector(".to-do-btn");
 todoButton.addEventListener("click", addtodo);
-const todoList=document.querySelector(".to-container")
+const todoList = document.querySelector(".to-container")
+todoList.addEventListener("click", checkRemove);
+
 function addtodo(e) {
     e.preventDefault();
     console.log("hi");
@@ -14,7 +16,28 @@ function addtodo(e) {
 `
     tododDiv.innerHTML = newTodo;
     todoList.appendChild(tododDiv);
+    inputValue.value = " ";
     
 
+}
+function checkRemove(e) {
+    const classList = [...e.target.classList];
+    const item = e.target;
+    if (classList[1]=== "fa-trash-alt") {
+        const todo = item.parentElement.parentElement;
+        todo.remove();
+        
+        
+    }
+    else if (classList[1] === "fa-check-square") {
+        const todo = item.parentElement.parentElement;
+        todo.classList.toggle("complited");
+        
+        
+        
+        
+        
+    }
+    
 }
  
